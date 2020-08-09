99bc57cc5389e3c38e89a43f1575f1f9807599e4
theprogrammer1 <69433557+theprogrammer1@users.noreply.github.com>
2020-08-09 20:28:00 +0100

    qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,4 +1,5 @@
- what text you want or a web address.
+import pyqrcode
+# enter what text you want or a web address.
 qr = pyqrcode.create('www.google.com')
-qr.png('abc1.png', scale=8)import pyqrcode
-# enter
+qr.png('abc1.png', scale=8)
+# You can then scan this with your phone.
