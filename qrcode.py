526cc2eb859b5466dbd861c17ac2bad9f522d6ae
theprogrammer1 <69433557+theprogrammer1@users.noreply.github.com>
2020-08-22 12:28:28 +0100

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,6 @@
-import pyqrcode
-# enter what text you want or a web address.
-qr = pyqrcode.create('www.google.com')
-qr.png('abc1.png', scale=8)
-# You can then scan this with your phone.
+from PIL import Image, ImageDraw
+import qrcode
+data="hello"
+image = qrcode.make(data)
+image.save("abc1231.png")
+# install the module like this pip install pillow qrcode
