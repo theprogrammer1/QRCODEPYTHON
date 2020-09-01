204b5aaf624385840718ee0efdb20960e6aab2f2
theprogrammer1 <69433557+theprogrammer1@users.noreply.github.com>
2020-09-01 15:39:13 +0100

    Create temperatureconverter.py

---
+++ temperatureconverter.py
@@ -0,0 +1,5 @@
+# Here's a temperature converter from Celsius to Fahrenheit
+celsius = int(input("Enter the temperature in Degrees celsius that you would like to convert: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print("The converted temperature is", fahrenheit)
