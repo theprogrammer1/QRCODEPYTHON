b636d27d84bf5b405ca953a88a5879cac016dad9
@theprogrammer1 <69433557+theprogrammer1@users.noreply.github.com>
2020-09-07 19:09:47 +0100

    Create watercheck.py

---
+++ watercheck.py
@@ -0,0 +1,13 @@
+# Just a simple water check!:)
+print("Welcome to WaterCheck")
+
+goal = input("How much ml of water do you plan to drink today? ")
+print("Come after a day and then enter how much you drank")
+water = input("How much ml of water have you drank today? ")
+
+if goal > water:
+	print("You have completed your goal")
+if goal < water:
+	print("You have not completed your goal")
+if goal == water :
+	print("You have completed your goal")
